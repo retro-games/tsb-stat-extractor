44385ef333b18b63b56e17dd1f261f4f97f480e7
edrex333 <edrex333@gmail.com>
2017-01-29 12:48:28 -0600

    player stats

---
+++ src/definitions/players/def-player-stats.js
@@ -0,0 +1,13 @@
+/**
+ * Created by edgrams on 1/29/17.
+ */
+
+class DefPlayerStats {
+    constructor() {
+        this.condition = undefined;
+        this.interceptions = undefined;
+        this.intTouchdowns = undefined;
+        this.intYards = undefined;
+        this.sacks = undefined;
+    }
+}
