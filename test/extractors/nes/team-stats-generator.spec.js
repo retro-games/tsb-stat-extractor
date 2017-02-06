7324d5583dcc2b72a19fe0683336af0e289d426e
edrex333 <edrex333@gmail.com>
2017-02-05 19:11:39 -0600

    team stats update

--- test/extractors/nes/team-stats-generator.spec.js
+++ test/extractors/nes/team-stats-generator.spec.js
@@ -2,14 +2,92 @@
  * Created by edgrams on 2/5/17.
  */
 
-import {generateTeamStats} from '../../../src/extractors/nes/team-stats-generator';
+jest.unmock('../../../src/utility');
+
+import generateTeamStats from '../../../src/extractors/nes/team-stats-generator';
+
+let bytes, locations, result;
 
 describe('team-stats-generator', function () {
     describe('generateTeamStats', function () {
-        test('should do stuff', () => {
-            let state = require('../../fixtures/nes/game_one/state.json');
-            console.log(atob(state.binary));
-            expect(true).toBe(true);
+        describe('home', function () {
+            beforeEach(() => {
+                bytes = new Uint8Array([8, 2, 0, 16, 7, 3, 32]);
+                locations = {
+                    FIRST_DOWNS: 0,
+                    SCORES: 2,
+                    TEAM_ID: 1
+                };
+                result = generateTeamStats(bytes, locations, undefined, true);
+            });
+
+            test('first downs', () => {
+                expect(result.firstDowns).toEqual(8);
+            });
+
+            test('team id', () => {
+                expect(result.teamId).toEqual(2);
+            });
+
+            test('first quarter score', () => {
+                expect(result.score.firstQuarter).toEqual(0);
+            });
+
+            test('second quarter score', () => {
+                expect(result.score.secondQuarter).toEqual(10);
+            });
+
+            test('third quarter score', () => {
+                expect(result.score.thirdQuarter).toEqual(7);
+            });
+
+            test('fourth quarter score', () => {
+                expect(result.score.fourthQuarter).toEqual(3);
+            });
+
+            test('final score', () => {
+                expect(result.score.finalScore).toEqual(20);
+            });
+        });
+
+        describe('away', function () {
+            beforeEach(() => {
+                bytes = new Uint8Array([10, 1, 16, 16, 0, 23, 55]);
+                locations = {
+                    FIRST_DOWNS: 0,
+                    SCORES: 2,
+                    TEAM_ID: 1
+                };
+                result = generateTeamStats(bytes, undefined, locations, false);
+            });
+
+            test('first downs', () => {
+                expect(result.firstDowns).toEqual(10);
+            });
+
+            test('team id', () => {
+                expect(result.teamId).toEqual(1);
+            });
+
+            test('first quarter score', () => {
+                expect(result.score.firstQuarter).toEqual(10);
+            });
+
+            test('second quarter score', () => {
+                expect(result.score.secondQuarter).toEqual(10);
+            });
+
+            test('third quarter score', () => {
+                expect(result.score.thirdQuarter).toEqual(0);
+            });
+
+            test('fourth quarter score', () => {
+                expect(result.score.fourthQuarter).toEqual(17);
+            });
+
+            test('final score', () => {
+                expect(result.score.finalScore).toEqual(37);
+            });
         });
     });
 });
