cf77529502582def8e5dcfce619369a4493ae44f
edrex333 <edrex333@gmail.com>
2017-02-11 15:11:20 -0600

    add eslint to build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var jshint = require("gulp-jshint");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
 
-gulp.task('lint', function () {
-   return gulp.src(['src/**/*.js', 'test/**/*.spec.js'])
+gulp.task("lint", function () {
+   return gulp.src(["src/**/*.js", "test/**/*.spec.js"])
        .pipe(jshint())
-       .pipe(jshint.reporter('default'));
+       .pipe(jshint.reporter("default"));
 });
 
-gulp.task('scripts', function () {
-   return gulp.src('src/**/*.js')
-       .pipe(concat('tsb-stat-extractor.js'))
-       .pipe(gulp.dest('dist'));
+gulp.task("scripts", function () {
+   return gulp.src("src/**/*.js")
+       .pipe(concat("tsb-stat-extractor.js"))
+       .pipe(gulp.dest("dist"));
 });
 
-gulp.task('default', ['lint', 'scripts']);
+gulp.task("default", ["lint", "scripts"]);
